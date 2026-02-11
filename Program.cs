468f89faae47b1be2665d87b6871dff07c520e53
jagadeeshreddiie <jagadeeshreddii4u@gmail.com>
2026-02-11 16:47:39 +0530

    Reuest pipeline Middleware setup using next() and Logger()

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 //using Microsoft.AspNetCore;
+//using Microsoft.AspNetCore;
 
 //namespace EmployeeManagement
 //{
@@ -22,15 +23,40 @@ var app=builder.Build();
 // IConfiguration is the builtin service to read the Configuration.
 IConfiguration config = app.Configuration;
 
+/* Minimal APIs Based on Endpoint
+
 // getting the Processing server name  eg: IIS - Inprocess, Kestrel - OutOfProcess
-app.MapGet("/", () => System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+//app.MapGet("/", () => System.Diagnostics.Process.GetCurrentProcess().ProcessName);
 
-app.MapGet("/user", () => "User Endpoint");
+//app.MapGet("/user", () => "User Endpoint");
 
-// Configuration
-// AppSettings -> AppSettings.development -> User Secrets -> Env Variables -> Cmd line Args.
-// cmd ex: dotnet run MyKey="Value from CMD"
+//// Configuration
+//// AppSettings -> AppSettings.development -> User Secrets -> Env Variables -> Cmd line Args.
+//// cmd ex: dotnet run MyKey="Value from CMD"
 
-app.MapGet("/Key", () => config["MyKey"]);
+//app.MapGet("/Key", () => config["MyKey"]);
+
+*/
+
+// Minimal Universal API with context delegate.
+app.Use(async (context,next) => {
+    // Use with next parameter will pass the control to next middleware, otherwise it will be terminated.
+
+    // logger
+
+    var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+
+    logger.LogInformation("MW1 : Request Processing");
+
+    await context.Response.WriteAsync(context.Request.Path + "  " + config["MyKey"]);
+
+    await next();
+
+    logger.LogInformation("MW1 : Response Processing");
+
+});
+
+app.Run( async (context) => { await context.Response.WriteAsync(context.Request.Method +"  "+ config["MyKey"]); });
+
 
 app.Run();
